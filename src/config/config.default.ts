06ec2caa7a3bfc715fd3652ba905945a8433bafb
Csmally <1130180692@qq.com>
2024-07-17 22:26:36 +0800

    feat: 更改端口号

--- src/config/config.default.ts
+++ src/config/config.default.ts
@@ -4,6 +4,6 @@ export default {
   // use for cookie sign key, should change to your own and keep security
   keys: '1721208587446_7360',
   koa: {
-    port: 7271,
+    port: 7002,
   },
 } as MidwayConfig;
